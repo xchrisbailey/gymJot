bec5329f6651b70057838a27e1ffae18efcc63cb
Chris Bailey <chris@ghostly.io>
2025-01-20 03:00:14 -0500

    ✨feat(sidebar): add dropdown menu for log navigation
    Split log navigation into two separate actions - view log and add log - using
    a dropdown menu component. Reordered exercise menu items for consistency.

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -6,6 +6,7 @@ import {
   List,
   LogIn,
   LogOut,
+  Pencil,
   Plus,
   User,
   UserPlus,
@@ -69,12 +70,29 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
               </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <Link href="/log">
-                  <FileText className="mr-2 h-4 w-4" />
-                  Log
-                </Link>
-              </SidebarMenuButton>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <FileText className="mr-2 h-4 w-4" />
+                    Log
+                    <ChevronDown className="ml-auto h-4 w-4" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <Link href="/log/view" className="flex">
+                      <List className="mr-2 h-4 w-4" />
+                      View Log
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href="/log" className="flex">
+                      <Pencil className="mr-2 h-4 w-4" />
+                      Add Log
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </SidebarMenuItem>
             <SidebarMenuItem>
               <DropdownMenu>
@@ -87,15 +105,15 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                   <DropdownMenuItem>
-                    <Link href="/exercises/new" className="flex">
-                      <Plus className="mr-2 h-4 w-4" />
-                      Add Exercise
-                    </Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
                     <Link href="/exercises" className="flex">
                       <List className="mr-2 h-4 w-4" />
                       View All
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href="/exercises/new" className="flex">
+                      <Plus className="mr-2 h-4 w-4" />
+                      Add Exercise
                     </Link>
                   </DropdownMenuItem>
                 </DropdownMenuContent>
